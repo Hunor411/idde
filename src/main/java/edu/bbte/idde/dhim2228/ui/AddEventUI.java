955a84edf2a713a94b85a97a0e32ce3c1d388955
Deak Hunor <deakhunor2002@gmail.com>
2024-10-25 11:06:58 +0300

    fix addeventui MC_OVERRIDABLE_METHOD_CALL_IN_CONSTRUCTOR?????

--- src/main/java/edu/bbte/idde/dhim2228/ui/AddEventUI.java
+++ src/main/java/edu/bbte/idde/dhim2228/ui/AddEventUI.java
@@ -13,34 +13,22 @@ import java.util.Locale;
 
 public class AddEventUI extends JFrame {
     private final EventService eventService;
-    private final EventManager parentUI;
 
-    private JTextField nameField;
-    private JTextField locationField;
-    private JTextArea descriptionArea;
-    private JTextField attendeesCountField;
-    private JComboBox<Integer> yearComboBox;
-    private JComboBox<String> monthComboBox;
-    private JComboBox<String> dayComboBox;
-    private JTextField hourField;
-    private JTextField minuteField;
-    private JCheckBox isOnlineCheckBox;
+    private final JTextField nameField;
+    private final JTextField locationField;
+    private final JTextArea descriptionArea;
+    private final JTextField attendeesCountField;
+    private final JComboBox<Integer> yearComboBox;
+    private final JComboBox<String> monthComboBox;
+    private final JComboBox<String> dayComboBox;
+    private final JTextField hourField;
+    private final JTextField minuteField;
+    private final JCheckBox isOnlineCheckBox;
     JButton saveButton;
 
     public AddEventUI(EventManager parentUI) {
         this.eventService = ServiceFactory.getInstance().getEventService();
-        this.parentUI = parentUI;
 
-        setupAndShowUI();
-    }
-
-    private void setupAndShowUI() {
-        setupUI();
-        initializeComponents();
-        this.setVisible(true);
-    }
-
-    private void setupUI() {
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int width = 400;
@@ -50,9 +38,7 @@ public class AddEventUI extends JFrame {
         this.setBounds(x, y, width, height);
         this.setTitle("Új esemény hozzáadása");
         this.setLayout(new GridLayout(10, 2));
-    }
 
-    private void initializeComponents() {
         nameField = new JTextField();
         locationField = new JTextField();
         descriptionArea = new JTextArea();
@@ -62,20 +48,6 @@ public class AddEventUI extends JFrame {
         monthComboBox = new JComboBox<>();
         dayComboBox = new JComboBox<>();
 
-        initializeDateComponents();
-
-        hourField = new JTextField();
-        minuteField = new JTextField();
-
-        isOnlineCheckBox = new JCheckBox("Online esemény");
-
-        saveButton = new JButton("Mentés");
-        saveButton.addActionListener(e -> handleSaveEvent());
-
-        addComponentsToUI();
-    }
-
-    private void initializeDateComponents() {
         for (int year = 2024; year <= 2030; year++) {
             yearComboBox.addItem(year);
         }
@@ -90,9 +62,66 @@ public class AddEventUI extends JFrame {
             dayComboBox.addItem(day.getDisplayName(java.time.format.TextStyle.FULL, Locale.forLanguageTag("hu")));
         }
         dayComboBox.setSelectedIndex(0);
-    }
 
-    private void addComponentsToUI() {
+        hourField = new JTextField();
+        minuteField = new JTextField();
+
+        isOnlineCheckBox = new JCheckBox("Online esemény");
+
+        saveButton = new JButton("Mentés");
+        saveButton.addActionListener(e -> {
+            try {
+                String name = nameField.getText();
+                String location = locationField.getText();
+                String description = descriptionArea.getText();
+                int attendeesCount = Integer.parseInt(attendeesCountField.getText());
+
+                if (name.isEmpty() || location.isEmpty() || description.isEmpty()
+                        || attendeesCountField.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(
+                            this,
+                            "Kérlek, tölts ki minden mezőt! ",
+                            "Hiba",
+                            JOptionPane.ERROR_MESSAGE
+                    );
+                    return;
+                }
+
+                int year = (Integer) yearComboBox.getSelectedItem();
+                int month = monthComboBox.getSelectedIndex() + 1;
+                int dayOfMonth = dayComboBox.getSelectedIndex() + 1;
+                int hour = Integer.parseInt(hourField.getText());
+                int minute = Integer.parseInt(minuteField.getText());
+
+                if (hour < 0 || hour > 23 || minute < 0 || minute > 59) {
+                    throw new NumberFormatException();
+                }
+
+                LocalDateTime date = LocalDateTime.of(year, month, dayOfMonth, hour, minute);
+                boolean isOnline = isOnlineCheckBox.isSelected();
+
+                EventModel newEvent = new EventModel(name, location, date, isOnline, description, attendeesCount);
+                eventService.createEvent(newEvent);
+
+                JOptionPane.showMessageDialog(this, "Esemény sikeresen hozzáadva!");
+                parentUI.fillTableWithEvents();
+                this.dispose();
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(
+                        this,
+                        "Kérlek, adj meg érvényes számokat az órához, perchez, vagy résztvevők számához.",
+                        "Hiba",
+                        JOptionPane.ERROR_MESSAGE
+                );
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(this,
+                        "Hiba történt az esemény létrehozásakor: " + ex.getMessage(),
+                        "Hiba",
+                        JOptionPane.ERROR_MESSAGE
+                );
+            }
+        });
+
         this.add(new JLabel("Esemény neve:"));
         this.add(nameField);
         this.add(new JLabel("Helyszín:"));
@@ -118,58 +147,6 @@ public class AddEventUI extends JFrame {
 
         this.add(isOnlineCheckBox);
         this.add(saveButton);
-    }
-
-    private void handleSaveEvent() {
-        try {
-            String name = nameField.getText();
-            String location = locationField.getText();
-            String description = descriptionArea.getText();
-            int attendeesCount = Integer.parseInt(attendeesCountField.getText());
-
-            if (name.isEmpty() || location.isEmpty() || description.isEmpty()
-                    || attendeesCountField.getText().isEmpty()) {
-                JOptionPane.showMessageDialog(this,
-                        "Kérlek, tölts ki minden mezőt! ",
-                        "Hiba",
-                        JOptionPane.ERROR_MESSAGE
-                );
-                return;
-            }
-
-            int year = (Integer) yearComboBox.getSelectedItem();
-            int month = monthComboBox.getSelectedIndex() + 1;
-            int dayOfMonth = dayComboBox.getSelectedIndex() + 1;
-            int hour = Integer.parseInt(hourField.getText());
-            int minute = Integer.parseInt(minuteField.getText());
-
-            if (hour < 0 || hour > 23 || minute < 0 || minute > 59) {
-                throw new NumberFormatException();
-            }
-
-            LocalDateTime date = LocalDateTime.of(year, month, dayOfMonth, hour, minute);
-
-            boolean isOnline = isOnlineCheckBox.isSelected();
-
-            EventModel newEvent = new EventModel(name, location, date, isOnline, description, attendeesCount);
-            eventService.createEvent(newEvent);
-
-            JOptionPane.showMessageDialog(this, "Esemény sikeresen hozzáadva!");
-            parentUI.fillTableWithEvents();
-            this.dispose();
-        } catch (NumberFormatException ex) {
-            JOptionPane.showMessageDialog(
-                    this,
-                    "Kérlek, adj meg érvényes számokat az órához, perchez, vagy résztvevők számához.",
-                    "Hiba",
-                    JOptionPane.ERROR_MESSAGE
-            );
-        } catch (Exception ex) {
-            JOptionPane.showMessageDialog(this,
-                    "Hiba történt az esemény létrehozásakor: " + ex.getMessage(),
-                    "Hiba",
-                    JOptionPane.ERROR_MESSAGE
-            );
-        }
+        this.setVisible(true);
     }
 }
