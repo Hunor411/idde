48a13f984a812f9a275e3c6e8bee5d435312bf75
Deak Hunor <deakhunor2002@gmail.com>
2024-11-02 22:12:10 +0200

    handling repository exceptions in serviceimp

--- src/main/java/edu/bbte/idde/dhim2228/service/implementation/EventServiceImp.java
+++ src/main/java/edu/bbte/idde/dhim2228/service/implementation/EventServiceImp.java
@@ -3,12 +3,15 @@ package edu.bbte.idde.dhim2228.service.implementation;
 import edu.bbte.idde.dhim2228.repository.DaoFactory;
 import edu.bbte.idde.dhim2228.repository.EventDao;
 import edu.bbte.idde.dhim2228.model.EventModel;
+import edu.bbte.idde.dhim2228.repository.exceptions.RepositoryException;
 import edu.bbte.idde.dhim2228.service.EventService;
 import edu.bbte.idde.dhim2228.service.exceptions.ServiceException;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
+@Slf4j
 public class EventServiceImp implements EventService {
     private final EventDao eventDao;
 
@@ -18,26 +21,51 @@ public class EventServiceImp implements EventService {
 
     @Override
     public EventModel getEventById(Long id) throws ServiceException {
-        return eventDao.getEventById(id);
+        try {
+            return eventDao.getEventById(id);
+        } catch (RepositoryException e) {
+            log.error("Error while getting event by id: {}", id, e);
+            throw new ServiceException("Error getting event by id: " + id, e);
+        }
     }
 
     @Override
     public Collection<EventModel> getAllEvents() {
-        return new ArrayList<>(eventDao.getAllEvents());
+        try {
+            return new ArrayList<>(eventDao.getAllEvents());
+        } catch (RepositoryException e) {
+            log.error("Error while retrieving all events", e);
+            throw new ServiceException("Error retrieving all events", e);
+        }
     }
 
     @Override
     public void createEvent(EventModel event) throws ServiceException {
-        eventDao.createEvent(event);
+        try {
+            eventDao.createEvent(event);
+        } catch (RepositoryException e) {
+            log.error("Error while creating event: {}", event, e);
+            throw new ServiceException("Error creating event", e);
+        }
     }
 
     @Override
     public void updateEvent(Long id, EventModel event) throws ServiceException {
-        eventDao.updateEvent(id, event);
+        try {
+            eventDao.updateEvent(id, event);
+        } catch (RepositoryException e) {
+            log.error("Error while updating event with id: {}", id, e);
+            throw new ServiceException("Error updating event with id: " + id, e);
+        }
     }
 
     @Override
     public void deleteEvent(Long id) throws ServiceException {
-        eventDao.deleteEvent(id);
+        try {
+            eventDao.deleteEvent(id);
+        } catch (RepositoryException e) {
+            log.error("Error while deleting event with id: {}", id, e);
+            throw new ServiceException("Error deleting event with id: " + id, e);
+        }
     }
 }
