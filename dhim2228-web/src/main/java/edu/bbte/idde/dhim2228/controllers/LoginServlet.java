cd020aa7c873f179db790baf756a78b399a143a6
Deak Hunor <deakhunor2002@gmail.com>
2024-11-06 14:41:43 +0200

    autfilter to /events-template and login page

---
+++ dhim2228-web/src/main/java/edu/bbte/idde/dhim2228/controllers/LoginServlet.java
@@ -0,0 +1,34 @@
+package edu.bbte.idde.dhim2228.controllers;
+
+import com.sun.tools.jconsole.JConsoleContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "admin";
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if (!USERNAME.equals(username) || !PASSWORD.equals(password)) {
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            resp.sendRedirect(req.getContextPath() + "/login.html");
+            return;
+        }
+
+        session.setAttribute("username", username);
+        resp.sendRedirect(req.getContextPath() + "/events-template");
+    }
+}
---
+++ dhim2228-web/src/main/java/edu/bbte/idde/dhim2228/filters/AuthFilter.java
@@ -0,0 +1,38 @@
+package edu.bbte.idde.dhim2228.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/events-template")
+public class AuthFilter extends HttpFilter {
+    private static final Logger log = LoggerFactory.getLogger(AuthFilter.class);
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpSession session = req.getSession();
+        String username = (String) session.getAttribute("username");
+
+        if (username == null) {
+            log.info("Unauthorized access attempt to {} from IP: {}. Redirecting to login.",
+                    req.getRequestURI(), req.getRemoteAddr());
+            res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            res.sendRedirect(req.getContextPath() + "/login.html");
+            return;
+        }
+
+        log.info("User '{}' accessed {}", username, req.getRequestURI());
+        chain.doFilter(req, res);
+    }
+
+}
