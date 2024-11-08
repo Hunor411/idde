f8b926af7acbeec2b87bf071f36fb366594b09d3
Deak Hunor <deakhunor2002@gmail.com>
2024-11-08 10:44:28 +0200

    pipeline fix

--- dhim2228-web/src/main/java/edu/bbte/idde/dhim2228/filters/LogFilter.java
+++ dhim2228-web/src/main/java/edu/bbte/idde/dhim2228/filters/LogFilter.java
@@ -2,8 +2,6 @@ package edu.bbte.idde.dhim2228.filters;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,7 +14,8 @@ import java.io.IOException;
 @WebFilter("/*")
 public class LogFilter extends HttpFilter {
     @Override
-    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
         super.doFilter(req, res, chain);
         log.info("HTTP Method: {}, URI: {}, Status Code: {}", req.getMethod(), req.getRequestURI(), res.getStatus());
     }
