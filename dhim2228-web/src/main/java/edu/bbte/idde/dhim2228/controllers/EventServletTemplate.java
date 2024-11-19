6d15e45ac96c266c033872801699e0733fd50f6e
Deak Hunor <deakhunor2002@gmail.com>
2024-11-19 12:17:15 +0200

    unused import fix

--- dhim2228-web/src/main/java/edu/bbte/idde/dhim2228/controllers/EventServletTemplate.java
+++ dhim2228-web/src/main/java/edu/bbte/idde/dhim2228/controllers/EventServletTemplate.java
@@ -15,10 +15,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
