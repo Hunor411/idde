c592d50441b37f2e7268703ce836b3335ff81db0
Deak Hunor <deakhunor2002@gmail.com>
2024-11-30 17:21:38 +0200

    unused import + id to mapper

--- dhim2228-spring/src/main/java/edu/bbte/idde/dhim2228/mapper/EventMapper.java
+++ dhim2228-spring/src/main/java/edu/bbte/idde/dhim2228/mapper/EventMapper.java
@@ -4,12 +4,14 @@ import edu.bbte.idde.dhim2228.dto.EventRequestDto;
 import edu.bbte.idde.dhim2228.dto.EventResponseDto;
 import edu.bbte.idde.dhim2228.model.Event;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.Collection;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface EventMapper {
+    @Mapping(target = "id", ignore = true)
     Event toEntityDto(EventRequestDto dto);
 
     EventResponseDto toResponseDto(Event entity);
--- dhim2228-spring/src/main/java/edu/bbte/idde/dhim2228/utils/Config.java
+++ dhim2228-spring/src/main/java/edu/bbte/idde/dhim2228/utils/Config.java
@@ -1,7 +1,6 @@
 package edu.bbte.idde.dhim2228.utils;
 
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
