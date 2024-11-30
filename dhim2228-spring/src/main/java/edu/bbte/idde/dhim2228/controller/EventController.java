399163f35ae4cb6a69f929c1adf31b8e9874c4aa
Deak Hunor <deakhunor2002@gmail.com>
2024-11-30 17:16:00 +0200

    controller: get all events

---
+++ dhim2228-spring/src/main/java/edu/bbte/idde/dhim2228/controller/EventController.java
@@ -0,0 +1,27 @@
+package edu.bbte.idde.dhim2228.controller;
+
+import edu.bbte.idde.dhim2228.dto.EventResponseDto;
+import edu.bbte.idde.dhim2228.mapper.EventMapper;
+import edu.bbte.idde.dhim2228.service.EventService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("api/events")
+@RequiredArgsConstructor
+public class EventController {
+    private final EventService eventService;
+    private final EventMapper eventMapper;
+
+    @GetMapping
+    public List<EventResponseDto> getAllEvents() {
+        log.info("Get all events");
+        return eventMapper.toResponseDtoList(eventService.findAll());
+    }
+}
--- dhim2228-spring/src/main/java/edu/bbte/idde/dhim2228/mapper/EventMapper.java
+++ dhim2228-spring/src/main/java/edu/bbte/idde/dhim2228/mapper/EventMapper.java
@@ -5,9 +5,14 @@ import edu.bbte.idde.dhim2228.dto.EventResponseDto;
 import edu.bbte.idde.dhim2228.model.Event;
 import org.mapstruct.Mapper;
 
+import java.util.Collection;
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface EventMapper {
     Event toEntityDto(EventRequestDto dto);
 
     EventResponseDto toResponseDto(Event entity);
+
+    List<EventResponseDto> toResponseDtoList(Collection<Event> entities);
 }
